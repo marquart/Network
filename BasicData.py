db18e5ce2a26e4ef95ae427122b7d288be56edfc
marquart <aron.marquart@gmx.de>
2017-03-06 17:43:18 +0100

    Add files via upload

---
+++ BasicData.py
@@ -0,0 +1,26 @@
+from tkinter import *
+import csv
+
+def writeBasicData():
+    csvfile = open('BasicData.csv', 'w', newline='')
+    csvfileWriter = csv.writer(csvfile)
+    csvfileWriter.writerow([Name.get()], [Geburtsjahr.get()])
+    
+    csvfile.close()
+def gui():
+    window = Tk()
+    Label(window, text="Name").grid(row=0)
+    Label(window, text="Geburtsjahr").grid(row=1)
+    
+    Name = Entry(window)
+    Geburtsjahr = Entry(window)
+    
+    Name.grid(row=0, column=1)
+    Geburtsjahr.grid(row=1, column=1)
+    
+    
+    endbutton = Button(window, text="Sichern", command=writeBasicData).grid(row=3, column=0, sticky=W, pady=4)
+    
+    
+    window.mainloop()
+gui()
